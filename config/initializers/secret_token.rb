3e78c2d00d263f97b4d33dc99bb7a1e254514682
EC2 Default User <ec2-user@ip-172-31-4-58.us-east-2.compute.internal>
2018-07-24 22:44:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '0228a326ba3660902e6de12f57088cfa8327a264b344a0d0acdcce67e635ab28df7ce7d84e652425a113b969bb3f82cd91540e7fa365372a007c87afc3ce2329'
